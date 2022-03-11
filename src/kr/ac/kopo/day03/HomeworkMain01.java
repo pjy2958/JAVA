85fb64d6c8c3cfe222edc9553fac360983648b48
JYP <pjy2958@naver.com>
2022-03-11 09:13:55 +0900

    과제01

---
+++ src/kr/ac/kopo/day03/HomeworkMain01.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.day03;
+
+//10번 과제
+/*
+ *  a > b > c
+ *  a > c > b
+ *  b > a > c
+ *  b > c > a
+ *  c > a > b
+ *  c > b > a
+ */
+
+public class HomeworkMain01 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
