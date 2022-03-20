245c25b4488462951764f9dc2044351ad05baad9
pjy29 <pjy29@DESKTOP-HQ0H24Q>
2022-03-20 16:53:38 +0900

    메소드 과제

--- src/kr/ac/kopo/assignment05/Calculator.java
+++ src/kr/ac/kopo/assignment05/Calculator.java
@@ -1,5 +1,30 @@
 package kr.ac.kopo.assignment05;
 
 public class Calculator {
-
+	void add(int num1, int num2) {	// 덧셈
+		System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+	}
+	void sub(int num1, int num2) {	// 뺄셈
+		System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+	}
+	void mul(int num1, int num2) {	// 곱셈
+		System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+	}
+	void div(int num1, int num2) {	// 나눗셈
+		System.out.printf("%d / %d = %.3f\n", num1, num2, (float) num1 / (float) num2);
+	}
+	void prime(int num) { // 소수판별
+		int cnt = 0;
+		boolean result;
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0)
+				cnt ++;
+		}
+		if (cnt == 2) {
+			result = true;
+		} else
+			result = false;
+		
+		System.out.println(num + " 소수체크 : " + result);
+	}
 }
--- src/kr/ac/kopo/assignment05/CalculatorMain.java
+++ src/kr/ac/kopo/assignment05/CalculatorMain.java
@@ -1,8 +1,25 @@
 package kr.ac.kopo.assignment05;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Calculator calculator = new Calculator();
 		
+		System.out.printf("첫번째 정수 입력 : ");
+		int num1 = Integer.parseInt(sc.nextLine());
+		System.out.printf("두번째 정수 입력 : ");
+		int num2 = Integer.parseInt(sc.nextLine());
+		
+		System.out.println();
+		calculator.add(num1, num2);
+		calculator.sub(num1, num2);
+		calculator.mul(num1, num2);
+		calculator.div(num1, num2);
+		System.out.println();
+		calculator.prime(num1);
+		calculator.prime(num2);
 	}
 }
