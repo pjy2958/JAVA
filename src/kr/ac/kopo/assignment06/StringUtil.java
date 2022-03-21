124ac39e66945a2211eae36783d398dcfeab9587
pjy29 <pjy29@DESKTOP-HQ0H24Q>
2022-03-21 22:03:16 +0900

    문자갯수, 문자제거 과제

---
+++ src/kr/ac/kopo/assignment06/StringUtil.java
@@ -0,0 +1,86 @@
+package kr.ac.kopo.assignment06;
+
+public class StringUtil {
+	boolean isUpperChar(char c) {
+		if((int)c >= 'A' && (int)c < 'Z' + 1)
+			return true;
+		else
+			return false;
+			
+	}
+	
+	boolean isLowerChar(char c) {
+		if(c >= 'a' && c < 'z' + 1)
+			return true;
+		else
+			return false;
+	}
+	
+	int max(int i, int j) {
+		if(i > j)
+			return i;
+		else
+			return j;
+	}
+	
+	int min(int i, int j) {
+		if(i < j)
+			return i;
+		else
+			return j;	
+	}
+	
+	String reverseString(String str) {
+		String reverseString = "";
+		int cnt = str.length();
+		
+		for(int i = 0; i < str.length(); i++) {
+			cnt--;
+			reverseString += str.charAt(cnt);
+		}
+		return reverseString;
+	}
+	
+	String toUpperString(String str) {
+		String upperString = "";
+		
+		for(int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+				upperString += (char)(str.charAt(i) - ('a' - 'A'));
+			} else
+				upperString += str.charAt(i);
+		}
+		return upperString;
+	}
+	
+	String toLowerString(String str) {
+		String lowerString = "";
+		
+		for(int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+				lowerString += (char)(str.charAt(i) + ('a' - 'A'));
+			} else
+				lowerString += str.charAt(i);
+		}
+		return lowerString;
+	}
+	
+	public static int checkChar(String strData, char ch) {
+		int cnt = 0;
+		for(int i = 0; i < strData.length(); i++) {
+			if(strData.charAt(i) == ch)
+				cnt++;
+		}
+		return cnt;
+	}
+	
+	public static String removeChar(String oriStr, char delChar) {
+		String newStr = "";
+		for(int i = 0; i < oriStr.length(); i++) {
+			if(oriStr.charAt(i) != delChar) {
+				newStr += oriStr.charAt(i);
+			}
+		}
+		return newStr;
+	}
+}
