4915b4ceaec996e41f2670d76f98004fd6f40ca1
JYP <pjy2958@naver.com>
2022-03-21 16:44:43 +0900

    오버라이딩&상속 학습

--- src/kr/ac/kopo/day08/extend/Employee.java
+++ src/kr/ac/kopo/day08/extend/Employee.java
@@ -3,7 +3,7 @@ package kr.ac.kopo.day08.extend;
 public class Employee {
 	String name;
 //	private String name;	// private 접근제한자는 상속받은 하위클래스도 못봄. => 오로지 자신의 클래스에서만 접근가능.
-//	protected String name;	// protected 접근제한자는 상속받은 하위클래스와 자신의 클래스만 접근가능.
+//	protected String name;	// protected 접근제한자는 상속받은 같은 패키지 내의 클래스만 접근가능, 다른패키지여도 상속받으면 접근가능.
 	int salary;
 	String grade;
 	
