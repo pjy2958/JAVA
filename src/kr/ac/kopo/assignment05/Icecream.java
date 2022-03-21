329df4d9466fb5ec32d801b300217d5cf47117ac
JYP <pjy2958@naver.com>
2022-03-21 10:56:26 +0900

    메소드 과제

---
+++ src/kr/ac/kopo/assignment05/Icecream.java
@@ -0,0 +1,11 @@
+package kr.ac.kopo.assignment05;
+
+public class Icecream {
+	String name;
+	int price;
+	
+	Icecream(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+}
---
+++ src/kr/ac/kopo/assignment05/IcecreamMarket.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.assignment05;
+
+public class IcecreamMarket {
+	Icecream[] iceArr;
+	
+	void open() {
+		System.out.println();
+	}
+}
