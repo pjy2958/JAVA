734485a14bac8c1e65973db9f89e056105a972bd
JYP <pjy2958@naver.com>
2022-03-29 13:36:51 +0900

    프로젝트

--- src/kr/ac/kopo/project01/Library.java
+++ src/kr/ac/kopo/project01/Library.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-	private List<Member> memberList = new ArrayList();	// 회원 리스트
-	private List<Book> bookList = new ArrayList();
+	private List<Member> memberList;	// 회원 리스트
+	private List<Book> bookList;	// 책 리스트
+	public Library() {
+		super();
+		this.memberList = new ArrayList();
+		this.bookList = new ArrayList();
+	}
 }
--- src/kr/ac/kopo/project01/Login.java
+++ src/kr/ac/kopo/project01/Login.java
@@ -1,5 +1,25 @@
 package kr.ac.kopo.project01;
 
-public class Login {
+import java.util.Scanner;
 
+public class Login {
+		private Scanner sc;
+		
+		
+	public Login() {
+			this.sc = new Scanner(System.in);
+		}
+
+
+	public void loginView() {
+		System.out.println("로그인-------------");
+		System.out.print("id : ");
+		String id = sc.nextLine();
+		System.out.print("passward : ");
+		int passward = Integer.parseInt(sc.nextLine());
+	}
+	
+	public void login(String id, int passward) {
+		
+	}
 }
