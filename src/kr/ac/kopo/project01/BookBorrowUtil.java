f29b6761eb22bd1bb6916359025320689f40c707
pjy2958 <pjy2958@naver.com>
2022-04-03 10:44:35 +0900

    bookborrow

--- src/kr/ac/kopo/project01/BookBorrowUtil.java
+++ src/kr/ac/kopo/project01/BookBorrowUtil.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.project01;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class BookBorrowUtil {
@@ -9,26 +10,57 @@ public class BookBorrowUtil {
         this.sc = new Scanner(System.in);
     }
 
-    public void bookBorrow(){
-        System.out.println("대출할 책 번호를 입력하세요 : ");
+    public void bookBorrow() {
+        System.out.println("* 대출할 책 번호를 입력하세요 : ");
         int bookNum = Integer.parseInt(sc.nextLine());
+        if(isBookBorrow(bookNum)){
+            System.out.println("** 대출중인 책입니다.");
+            return;
+        }
+        int pivot = bookPivot();
+        if(pivot == 10) {
+            System.out.println("** 책을 더이상 대출할 수 없습니다.");
+            return;
+        }
+        if(pivot == 0) {
+            int[] bookBorrowArr = new int[10];
+            bookBorrowArr[0] = bookNum;
+            BookManagement.bookBorrowMap.put(LibraryManagement.loginMember.getMemberNumber(), bookBorrowArr);
+            return;
+        }
+        // 생성되어있는 map에 책넣기
+
+
+
+    }
+    public int bookPivot(){
+        Map <Integer, int[]> bookMap = BookManagement.bookBorrowMap;
+        if(bookMap == null)
+            return 0;
+        if(!bookMap.containsKey(LibraryManagement.loginMember.getMemberNumber())) {
+           return 0;
+       } else {
+            for (Integer key : bookMap.keySet()) {      // 아마 keyset오류생길듯
+                for (int i = 0; i < bookMap.get(key).length; i += 2) {
+                    if (bookMap.get(key)[i] == 0)
+                        return i;
+                }
+            }
+        }
+        return 10;
     }
 
     // 챽이 대여 중인지 확인하는 기능  대여중이면 true
     public boolean isBookBorrow(int bookNum) {
-        boolean borrowCheck = false;
-//        BookManagement.bookBorrowMap.forEach((key, value) -> {
-//            if(value.length == 0){
-//                if(Integer.valueOf(value[0]) == bookNum)
-////                    borrowCheck = true;
-//            } else {
-//                for (int i = 0; i < value.length; i+=2) {
-//                    if (Integer.valueOf(value[i]) == bookNum)
-////                        borrowCheck = true;
-//                }
-//            }
-//        });
-        borrowCheck = true;
-        return borrowCheck;
+        Map <Integer, int[]> bookMap = BookManagement.bookBorrowMap;
+        if(bookMap == null)
+            return false;
+        for(Integer key : bookMap.keySet()){
+            for(int i = 0; i < bookMap.get(key).length; i+=2){
+                if(bookMap.get(key)[i] == bookNum)
+                    return true;
+            }
+        }
+        return false;
     }
 }
--- src/kr/ac/kopo/project01/BookManagement.java
+++ src/kr/ac/kopo/project01/BookManagement.java
@@ -7,11 +7,9 @@ import java.util.Map;
 
 public class BookManagement {
     public static List<Book> bookList; // 책 리스트
-    private String[] borrowList;  // 빌린책번호, 빌린날짜를 저장하는 배열
-    public static Map<Integer, String[]> bookBorrowMap; // 회원번호-빌린책 HashMap
+    public static Map<Integer, int[]> bookBorrowMap; // 회원번호-빌린책 HashMap
     BookManagement(){
         this.bookList = new ArrayList();
-        this.borrowList = new String[10]; // 개인당 총5권까지만 대출가능
         this.bookBorrowMap = new HashMap<>();
     }
 }
